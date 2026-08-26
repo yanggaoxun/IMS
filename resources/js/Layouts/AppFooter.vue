a6ee7b28358fc82d2f4836c12ee681d8c4764429
yanggaoxun <yanggaoxun@qq.com>
2026-08-26 17:23:27 +0800

    chore(layout): remove brand text from footer

--- resources/js/Layouts/AppFooter.vue
+++ resources/js/Layouts/AppFooter.vue
@@ -1,10 +1,3 @@
-<script setup>
-import { useI18n } from 'vue-i18n';
-
-const { t } = useI18n();
-</script>
-
 <template>
-    <div class="layout-footer">
-        {{ t('brand.shortName') }}</div>
+    <div class="layout-footer"></div>
 </template>
